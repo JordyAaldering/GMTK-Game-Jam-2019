62ee753bc69e10bdb00db1ad673fb1e4476620b8
Jordy Aaldering <aalderingjordy@gmail.com>
2019-08-03 01:29:34 +0200

    Movement 1

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,23 +1,28 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Player
 {
     public class PlayerMovement : MonoBehaviour
     {
+        private SpriteRenderer _sr;
         private Animator _anim;
 
         private void Awake()
         {
+            _sr = GetComponentInChildren<SpriteRenderer>();
             _anim = GetComponentInChildren<Animator>();
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                _anim.SetBool("IsWalking", !_anim.GetBool("IsWalking"));
-            }
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            
+            Vector3 movement = new Vector3(horizontal, vertical, 0f);
+            transform.Translate(movement * Time.deltaTime);
+
+            _sr.flipX = horizontal > 0f;
+            _anim.SetBool("IsWalking", movement != Vector3.zero);
         }
     }
 }
