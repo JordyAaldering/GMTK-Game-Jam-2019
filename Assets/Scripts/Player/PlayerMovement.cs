113f91277c0e23f35c8b908108e7aaa6c63aa0da
Jordy Aaldering <aalderingjordy@gmail.com>
2019-08-03 01:14:17 +0200

    Cleanup, Sprites

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        private Animator _anim;
+
+        private void Awake()
+        {
+            _anim = GetComponentInChildren<Animator>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _anim.SetBool("IsWalking", !_anim.GetBool("IsWalking"));
+            }
+        }
+    }
+}
