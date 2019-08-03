a7786d1733a90e7de835e0624b903ffb4041fb94
Jordy Aaldering <aalderingjordy@gmail.com>
2019-08-03 20:14:55 +0200

    Effect

--- Assets/Scripts/UI/GameOverPanel.cs
+++ Assets/Scripts/UI/GameOverPanel.cs
@@ -1,10 +1,21 @@
-﻿using UnityEngine;
+﻿#pragma warning disable 0649
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace UI
 {
     public class GameOverPanel : MonoBehaviour
     {
+        [SerializeField] private GameObject _heart;
+        [SerializeField] private GameObject _heartDestroyEffect;
+
+        private void Awake()
+        {
+            _heart.SetActive(false);
+            Vector2 pos = Camera.main.ScreenToWorldPoint(_heart.transform.position);
+            Instantiate(_heartDestroyEffect, pos, Quaternion.identity);
+        }
+
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
